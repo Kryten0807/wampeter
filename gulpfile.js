29ff43e0d167bc43eeccc73fe1bc6d3f24e40e5d
Dave Thomas <dave@monstergfx.com>
2016-03-17 06:43:52 -0700

    added gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp   = require('gulp');
+
+var coffee = require('gulp-coffee');
+
+
+gulp.task('build', function () {
+    gulp.src('./src/wampeter/**/*.coffee')
+        .pipe(coffee({bare: true}))
+        .pipe(gulp.dest('./lib'));
+});
+
+
+gulp.task('watch', function () {
+    // watch the source files
+    //
+    gulp.watch('./src/app/**/*.coffee', ['server']);
+});
+
+
+gulp.task('default', ['build']);
