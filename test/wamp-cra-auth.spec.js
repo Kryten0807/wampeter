0c527c4df8634b5cf4221cf7b6c407067583cac3
Dave Thomas <dave@monstergfx.com>
2016-03-18 12:53:23 -0700

    rebuilt

--- test/wamp-cra-auth.spec.js
+++ test/wamp-cra-auth.spec.js
@@ -24,24 +24,28 @@
   chai.use(spies).use(promised);
 
   describe('Router:Session', function() {
-    var INVALID_KEY, VALID_KEY, connection, router, session;
+    var AUTHID, INVALID_KEY, VALID_KEY, connection, router, session;
     router = null;
     connection = null;
     session = null;
+    AUTHID = 'j.smith';
     VALID_KEY = 'abc123';
     INVALID_KEY = 'xyz789';
     before(function(done) {
+      var obj;
       router = wampeter.createRouter({
         port: 3000,
         auth: {
           wampcra: {
             type: 'static',
-            users: {
-              'alpha': {
+            users: (
+              obj = {},
+              obj["" + AUTHID] = {
                 secret: VALID_KEY,
                 role: 'frontend'
-              }
-            }
+              },
+              obj
+            )
           }
         }
       });
@@ -50,62 +54,82 @@
       }), 500);
     });
     after(function(done) {
-      if ((connection != null) && connection.isOpen) {
-        connection.close();
-      }
       return setTimeout(function() {
         return router.close().then(done)["catch"](done).done();
       });
     });
-    it('should establish a new session via static wamp-cra authentication', function(done) {
+    return it('should establish a new session via static wamp-cra authentication', function(done) {
+      var onchallenge;
       router.createRealm('com.to.inge.world');
+      onchallenge = function(session, method, extra) {
+        expect(method).to.equal('wampcra');
+        return autobahn.auth_cra.sign(VALID_KEY, extra.challenge);
+      };
       connection = new autobahn.Connection({
         realm: 'com.to.inge.world',
         url: 'ws://localhost:3000/wampeter',
-        authmethods: ['wampcra']
+        authmethods: ['wampcra'],
+        authid: AUTHID,
+        onchallenge: onchallenge
       });
-      connection.onchallenge = function(session, method, extra) {
-        expect(method).to.be('wampcra');
-        autobahn.auth_cra.sign(VALID_KEY, extra.challenge);
-        return done();
-      };
       connection.onopen = function(s) {
         expect(s).to.be.an["instanceof"](autobahn.Session);
         expect(s.isOpen).to.be["true"];
-        return session = s;
-      };
-      return connection.open();
-    });
-    it('should fail to establish a new session via static wamp-cra authentication', function(done) {
-      router.createRealm('com.to.inge.world');
-      connection = new autobahn.Connection({
-        realm: 'com.to.inge.world',
-        url: 'ws://localhost:3000/wampeter',
-        authmethods: ['wampcra']
-      });
-      connection.onchallenge = function(session, method, extra) {
-        expect(method).to.be('wampcra');
-        return autobahn.auth_cra.sign(INVALID_KEY, extra.challenge);
-      };
-      connection.onopen = function(s) {
-        expect(s).to.be.an["instanceof"](autobahn.Session);
-        expect(s.isOpen).to.be["true"];
-        return session = s;
-      };
-      connection.onerror = function(err) {
-        expect(err.type).to.be('wamp.error.not_not_authorized');
+        session = s;
         return done();
       };
       return connection.open();
     });
-    return it('should close a session', function(done) {
-      expect(connection).to.be.an["instanceof"](autobahn.Connection);
-      connection.onclose = function(reason) {
-        expect(reason).to.be.equal('closed');
-        return done();
-      };
-      return connection.close();
-    });
+
+    /*
+    it('should fail to establish a new session via static wamp-cra authentication', (done)->
+        router.createRealm('com.to.inge.world')
+    
+    
+        onchallenge = (session, method, extra)->
+    
+            expect(method).to.equal('wampcra')
+    
+             * respond to the challenge
+             *
+            autobahn.auth_cra.sign(INVALID_KEY, extra.challenge)
+    
+        connection = new autobahn.Connection({
+            realm: 'com.to.inge.world'
+            url: 'ws://localhost:3000/wampeter'
+    
+            authmethods: ['wampcra']
+            authid: AUTHID
+            onchallenge: onchallenge
+        })
+    
+    
+        connection.onopen = (s)->
+            expect(s).to.be.an.instanceof(autobahn.Session)
+            expect(s.isOpen).to.be.true
+            session = s
+             * done()
+    
+        connection.onerror = (err)->
+            expect(err.type).to.be('wamp.error.not_not_authorized')
+    
+            done()
+    
+        connection.open()
+    )
+     */
+
+    /*
+    it('should close a session', (done)->
+        expect(connection).to.be.an.instanceof(autobahn.Connection)
+    
+        connection.onclose = (reason)->
+            expect(reason).to.be.equal('closed')
+            done()
+    
+        connection.close()
+    )
+     */
   });
 
 }).call(this);
