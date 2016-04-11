677011d00f47672ed0b5ea1ba59e97bc0f7953e7
Dave Thomas <dave@monstergfx.com>
2016-04-11 06:31:02 -0700

    updated gulpfile to remove reference to mocha tests

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,19 @@ var coffee = require('gulp-coffee');
 
 
 gulp.task('build', function () {
+
     // build the library code
     //
     gulp.src('./src/wampeter/**/*.coffee')
         .pipe(coffee({bare: false}))
         .pipe(gulp.dest('./lib'));
 
-    // build the Mocha test code
+    // build the test code
     //
     gulp.src('./src/tests/**/*.coffee')
         .pipe(coffee({bare: false}))
         .pipe(gulp.dest('./test'));
 
-    // build the Tape test code
-    //
-    gulp.src('./src/tape/**/*.coffee')
-        .pipe(coffee({bare: false}))
-        .pipe(gulp.dest('./test'));
 });
 
 
